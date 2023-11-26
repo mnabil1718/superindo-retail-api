c9e33273457fbf5bc2381d496be30831aa4fc841
mnabil1718 <mnabil1718@gmail.com>
2023-11-27 01:56:58 +0700

    Fix url hint at root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   return (
     <div className="relative mx-auto max-w-screen-xl p-3">
       <p>Superindo Backend REST API site. To start, go to:</p>
-      <p>https://retail-api.vercel.app/api/products</p>
+      <p>https://superindo-retail-api.vercel.app/api/products</p>
     </div>
   );
 }
